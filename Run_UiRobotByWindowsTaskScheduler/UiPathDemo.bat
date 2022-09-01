a49e6b6b8e1e9eb747fd5074d7ae93f3678fd218
Vikram <45116392+vikram7sarun@users.noreply.github.com>
2022-09-01 23:55:09 +0530

    UiPath All Code
    UiPath All Code

---
+++ Run_UiRobotByWindowsTaskScheduler/UiPathDemo.bat
@@ -0,0 +1,1 @@
+"C:\Users\ADMIN\AppData\Local\Programs\UiPath\Studio\UiRobot.exe" -file "C:\Users\ADMIN\Documents\UiPath\Run_UiRobotByWindowsTaskScheduler\Run_UiRobotByWindowsTaskScheduler.1.0.3.nupkg"
